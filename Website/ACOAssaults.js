2e758c3db206ec90fea7f3605b67760e9c5beb42
Jonathan Clarke <jonathan@bitwix.com>
2010-01-20 16:54:00 +0000

    AJC tidying up a solution

---
+++ Website/ACOAssaults.js
@@ -0,0 +1,72 @@
+﻿// JScript File
+
+$(document).ready(function(){
+    InitNavMenu();
+    $(".userinput")
+        .focus( HighlightOnFocus )
+        .blur( HighlightOffOnBlur )
+        .blur( ClearHelpText )
+        .blur( DoSearch );
+    
+    $("#txtBorough").focus( function() { SetHelpText( $("#hlpBorough").text() ); } );
+    $("#txtFrequency").focus( function() { SetHelpText( $("#hlpFrequency").text() ); } );
+    
+    $("#txtBorough").focus();
+    Sayuser( "Ready" );
+    
+    $("#addone").click( function() { AddOne(); } );
+});
+
+function AddOne()
+{
+    DebugSayuser( "Add one" );
+}
+
+function DoSearch()
+{
+    FetchResults( $("#txtBorough").val(), $("#txtFrequency").val() );
+}
+
+function FetchResults( borough, frequency )
+{
+    var data, callback;
+    data = {borough: borough, frequency:frequency };
+    callback = function(xml) { DisplayResults( xml ); };
+    $.post("ACOAssaults.ashx", data, callback ); 
+    Sayuser( "Searching for " + borough + "/" + frequency );
+}
+
+function DisplayResults( xml )
+{
+    var msg = $("grail", xml).text() + " gave " + $("count", xml).text() + " results";
+    var count = 0;
+    Sayuser( msg );
+    
+    
+    $("#details").text( "" );
+    $("#details").append('<ol id="appended">');
+    $("ward",xml).each(function(i) {
+        $("#appended").append( "<li>" + $(this).find("name").text() 
+            + " (" + $(this).find("total").text() + ")</li>" );
+        count++;
+    });
+}
+
+function ClearHelpText()
+{
+    SetHelpText( "" );
+}
+
+function SetHelpText( msg )
+{
+    $("#lblHelp").text( msg );
+}
+
+function HighlightOnFocus()
+{
+    $(this).addClass( "HasFocus" );
+}
+function HighlightOffOnBlur()
+{
+    $(this).removeClass( "HasFocus" );
+}
---
+++ Website/Shared.js
@@ -0,0 +1,17 @@
+﻿// JScript File
+
+function InitNavMenu()
+{
+    var delim = "&nbsp;:&nbsp;";
+    var str = delim + "<a href='ACOAssaults.htm'>ACOAssaults</a>" + delim;
+    $("#navmenu").html( str );
+}
+
+function Sayuser( msg )
+{
+    $("#lblSayuser").text( msg );
+}
+function DebugSayuser( msg )
+{
+    $("#lblDebugSayuser").text( msg );
+}
