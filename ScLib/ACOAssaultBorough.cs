738533c1d4292f06957af41923079d181cdbef82
hardnut <hardnut@hardnut.com>
2010-01-22 08:49:25 +0000

    "ACOAssaultsXmlFile" appSetting now a relative path refactored DisplayResults function in ACOAssaults.js new ACOAssaults1.aspx page in Website with classes ACOAssaultBoroughs & ACOAssaultBorough in ScLib minor tinkerings elsewhere...

---
+++ ScLib/ACOAssaultBorough.cs
@@ -0,0 +1,20 @@
+namespace ScLib
+{
+    public class ACOAssaultBorough
+    {
+        string _boroughName;
+				int _assaultCount, _wardCount;
+
+				public ACOAssaultBorough(string boroughName, int assaultCount, int wardCount)
+        {
+            _boroughName = boroughName;
+						_assaultCount = assaultCount;
+						_wardCount = wardCount;
+        }
+
+				public string BoroughName { get { return _boroughName; } }
+				public int AssaultCount { get { return _assaultCount; } }
+				public int WardCount { get { return _wardCount; } }
+
+    }
+}
