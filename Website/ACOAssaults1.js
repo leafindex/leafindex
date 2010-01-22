539fb0ed77843ac383e09113315fd955114b8080
hardnut <hardnut@hardnut.com>
2010-01-22 15:26:38 +0000

    sortableTable for ACOAssaults1

--- Website/ACOAssaults1.js
+++ Website/ACOAssaults1.js
@@ -1,21 +1,78 @@
-﻿$(function(){
+﻿function sortableTable(table){
+	$('thead th',table).each(function(column){
+    var getSortKey;
+		if($(this).hasClass("number")){
+			getSortKey=function(cell){
+				console.log(cell.text().replace(/^[^d.]*/,""));
+				var key=parseFloat(cell.text().replace(/^[^d.]*/,""));
+				console.log(key);
+				return isNaN(key)?0:key;
+			};
+		}
+		else{
+			if($(this).hasClass("date")){
+	      getSortKey=function(cell) {
+	        return Date.parse(cell.text());
+				};
+			}
+			else{
+				getSortKey=function(cell){
+					return cell.text().toUpperCase();
+				};
+			}
+		}
+		$(this).click(function(){
+			var t=$(this);
+			var direction=(t.data("sort")==1)?-1:1;
+			t.data("sort",direction);
+      var rows=table.find("tbody > tr").get();
+      $.each(rows,function(i,row){
+	      row.sortKey=getSortKey($(row).children("td:eq("+column+")"));
+      });
+      rows.sort(function(a,b){
+				return (a.sortKey<b.sortKey)?-direction:((a.sortKey>b.sortKey)?direction:0);
+      });
+      $.each(rows,function(i,row){
+	      table.children("tbody").append(row);
+        row.sortKey=null;
+      });
+    });
+	});
+}
+function setupTable(t){
+	t=$(t);
+	var td=t.find("thead > tr > th");
+	td.filter(".number").each(function(){
+		t.find("tbody > tr > td:nth-child("+(td.index(this)+1)+")").addClass("number");
+	});
+	sortableTable(t);
+}
+$(function(){
 	$("#boroughSearch")
-		.liveUpdate("#boroughTable")
+		.tableSearch("#boroughTable")
 		.focus();
 	setupTable("#boroughTable");
 });
-function setupTable(t){
-	t=$(t);
-	var td=t.find("thead tr th");
-	td.filter(".number").each(function(){
-		t.find("tbody tr td:nth-child("+(td.index(this)+1)+")").addClass("number");
-	});
-}
-$.fn.liveUpdate=function(list){
-	list=$(list);
-	if(list.length){
-		var rows=list.find("tbody tr");
-		var	cache=rows.find("td:first").map(function(){
+$.fn.tableSearch=function(table){
+	var rows,cache;
+	function filter(){
+		var term=$.trim($(this).val().toLowerCase());
+		if(!term){
+			rows.show();
+		}
+		else{
+			rows.hide();
+			cache.each(function(i){
+				if(this.indexOf(term)>-1){
+					$(rows[i]).show();
+				}
+			});
+		}
+	}
+	table=$(table);
+	if(table.length){
+		rows=table.find("tbody > tr");
+		cache=rows.find("td:first").map(function(){
 				return $(this).text().toLowerCase();
 			});
 		this
@@ -25,18 +82,4 @@ $.fn.liveUpdate=function(list){
 			});
 	}
 	return this;
-		
-	function filter(){
-		var term=$.trim($(this).val().toLowerCase()),scores=[];
-		if(!term){
-			rows.show();
-		}else{
-			rows.hide();
-			cache.each(function(i){
-				if(this.indexOf(term)>-1){
-					jQuery(rows[i]).show();
-				}
-			});
-		}
-	}
 };
