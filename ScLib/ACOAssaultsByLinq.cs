da89b0f6031d98223aa3a97e57c5a019ac58d70a
Jonathan Clarke <jonathan@bitwix.com>
2010-01-25 17:52:01 +0000

    AJC refactoring LINQ using myWard

--- ScLib/ACOAssaultsByLinq.cs
+++ ScLib/ACOAssaultsByLinq.cs
@@ -23,22 +23,19 @@ namespace ScLib
                 results = GetWardsNumericRange(borough, fp.From, fp.To);
             else
                 results = GetWardsTopBottom(borough, fp.Count, fp.DoTop);
+
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(@"c:\temp\xx.txt", true))
+            {
+                sw.WriteLine(results.Count + " returned by " + borough + "/" + frequency);
+                sw.Close();
+            }
+
             return results.ToArray();
         }
 
         private List<ACOAssaultWard> GetWardsTopBottom(string borough, int count, bool do_top)
         {
-            XDocument xdoc = XDocument.Load(_xmlfilename);
-            var wards = from w in xdoc.Descendants("ROW")
-                        where w.Element("District_Name").Value.ToUpper().Contains(borough)
-                        orderby MyIntParse(w.Element("Count_12-2007_to_11-2009").Value) descending
-                        select new
-                        {
-                            ward = w.Element("Ward_Name").Value,
-                            borough = w.Element("District_Name").Value,
-                            incidents = MyIntParse(w.Element("Count_12-2007_to_11-2009").Value)
-                        };
-
+            IEnumerable<myWard> wards = GetWardsInBorough(borough);
             if (wards.Count() > count)
             {
                 if (!do_top)
@@ -55,20 +52,9 @@ namespace ScLib
             return results;
         }
 
-
         private List<ACOAssaultWard> GetWardsNumericRange(string borough, int from, int to)
         {
-            XDocument xdoc = XDocument.Load(_xmlfilename);
-            var wards = from w in xdoc.Descendants("ROW")
-                        where w.Element("District_Name").Value.ToUpper().Contains(borough)
-                        orderby MyIntParse(w.Element("Count_12-2007_to_11-2009").Value) descending
-                        select new
-                        {
-                            ward = w.Element("Ward_Name").Value,
-                            borough = w.Element("District_Name").Value,
-                            incidents = MyIntParse(w.Element("Count_12-2007_to_11-2009").Value)
-                        };
-
+            IEnumerable<myWard> wards = GetWardsInBorough(borough);
             List<ACOAssaultWard> results = new List<ACOAssaultWard>();
             foreach (var ward in wards)
             {
@@ -77,6 +63,44 @@ namespace ScLib
             }
             return results;
         }
+
+        public class myWard // : Object
+        {
+            public string ward;
+            public string borough;
+            public int incidents;
+        }
+
+        private IEnumerable<myWard> GetWardsInBorough(string borough)
+        {
+            XDocument xdoc = XDocument.Load(_xmlfilename);
+            return from w in xdoc.Descendants("ROW")
+                   where w.Element("District_Name").Value.ToUpper().Contains(borough)
+                   orderby MyIntParse(w.Element("Count_12-2007_to_11-2009").Value) descending
+                   select new myWard()
+                   {
+                       ward = w.Element("Ward_Name").Value,
+                       borough = w.Element("District_Name").Value,
+                       incidents = MyIntParse(w.Element("Count_12-2007_to_11-2009").Value)
+                   };
+        }
+
+
+        //private object GetWardsInBorough(string borough)
+        //{
+        //    XDocument xdoc = XDocument.Load(_xmlfilename);
+        //    var wards = from w in xdoc.Descendants("ROW")
+        //                where w.Element("District_Name").Value.ToUpper().Contains(borough)
+        //                orderby MyIntParse(w.Element("Count_12-2007_to_11-2009").Value) descending
+        //                select new
+        //                {
+        //                    ward = w.Element("Ward_Name").Value,
+        //                    borough = w.Element("District_Name").Value,
+        //                    incidents = MyIntParse(w.Element("Count_12-2007_to_11-2009").Value)
+        //                };
+
+        //    return wards;
+        //}
 
         private int MyIntParse(string str)
         {
