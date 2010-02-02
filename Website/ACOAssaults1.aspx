00fab5af181323b69338374f56955dea317acf30
hardnut <hardnut@hardnut.com>
2010-02-02 22:01:18 +0000

    much tarting

--- Website/ACOAssaults1.aspx
+++ Website/ACOAssaults1.aspx
@@ -3,14 +3,15 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"> 
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
-	<title>LeafIndex: ACOAssaults</title>
+	<title>LeafIndex: Ambulance Call Outs to Assaults - Summary</title>
   <link href="Shared.css" rel="stylesheet" type="text/css" />
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.0/jquery.min.js"></script>
-  <script type="text/javascript" src="Shared.js"></script>
-  <script type="text/javascript" src="ACOAssaults1.js"></script>
+  <script type="text/javascript" src="/js/Shared.js"></script>
+  <script type="text/javascript" src="/js/ACOAssaults1.js"></script>
 </head>
 <body>
 	<h2>Ambulance Call Outs to Assault Incidents</h2>
+	<ul class="linkList"><li>Summary</li><li><a href='ACOMapR.aspx'>Map</a></li><li><a href='ACOAssaults.htm'>Query</a></li></ul><br class="clear"/>
 	<form action="">
 		<div>
 		<label for="boroughSearch">Borough: </label><input type="text" id="boroughSearch"/>
--- Website/js/Shared.js
+++ Website/js/Shared.js
@@ -1,4 +1,66 @@
-﻿$.fn.superTable=function(options){
+﻿ function makeNavMenu() {
+	var m=$("<div id='navmenu'/>")
+		.html("<div id='navmenuLogo'><a href='Home.htm'><img src='images/logo2.png' alt='Logo' width='32' height='31' border='0'/></a></div>"
+			+ "<div id='navmenuLinks'><a href='Home.htm'>Home</a>"
+			+ "<a href='#' class='dropdownMenuing assaults' id='assaults'>Ambulance Call Outs To Assaults</a>"
+			+ "<a href='PopulationCrash.htm'>Population Crash</a>"
+			+ "<a href='http://leafindex.blogspot.com'>Blog</a>"
+			+ "<a href='Credits.htm'>Credits</a>"
+			+ "</div>"
+			+ "<div id='navmenuName'><a href='Home.htm'>LeafIndex</a></div>"
+			+ "<br class='clear'/>");
+	makeDropdownMenu(
+		m.find(".dropdownMenuing").filter(".assaults"),
+			"<a href='ACOAssaults1.aspx'>Summary</a>","<a href='ACOMapR.aspx'>Map</a>","<a href='ACOAssaults.htm'>Query</a>");
+	m.prependTo("body");
+	$("<div id='bottombar'/>").html("&copy;2010 Chris Heywood / Jonathan Clarke" ).appendTo("body");
+}
+function makeDropdownMenu(menuLink){
+	var args=arguments;
+	menuLink
+		.wrapInner("<span class='dropdownMenuLink'/>")
+		.append("<span class='dropdownMenuThing'>▼</span>")
+		.click(function(){
+			dropdownMenu.apply(this,args);
+			return false;
+		});
+}
+function dropdownMenu(menuLink){
+	var i=arguments.length;
+	if(i<=1){
+		return;
+	}
+	var id=menuLink.attr("id");
+	if(!id){
+		return;
+	}
+	var d=$("#dropdownMenu"+id);
+	if(d.length){
+		d.remove();
+		return;
+	}
+	d=$("<div id=\"dropdownMenu"+id+"\"/>")
+		.addClass("dropdownMenu")
+		.hide();
+	for(var j=1;j<i;j++){
+    d.append(arguments[j]);
+  }
+  var o=menuLink.offset();
+  d.appendTo("body")
+  d
+		.css({"left":(o.left+menuLink.outerWidth()-d.outerWidth())+"px","top":(o.top+menuLink.outerHeight()+5)+"px"})
+		.show();
+	$(document).bind("mousedown.dropdownMenu",function(e){
+		var t=$(e.target);
+		if(!t.is(".dropdownMenuing,.dropdownMenuLink,.dropdownMenuThing")){
+			if(!t.closest("div.dropdownMenu").length){
+				$("div.dropdownMenu").remove();
+				$(document).unbind(".dropdownMenu");
+			}
+		}
+	});
+}
+$.fn.superTable=function(options){
 	return this.each(function(){
 		var table,rows,colCnt,subRowCache,hasSubRowCache,reHeight=false;
 		function uid() {
@@ -171,11 +233,11 @@
 				setColCnt();
 				var c=parseInt(colsToUse,10);
 				c=isNaN(c)||c>colCnt?colCnt:c;
-				var prefix="<tr><td"+(c>1?" colspan=\""+c+"\"":"")+"><div class=\"subTableClose\"><a href=\"#\">▲</a></div>";
+				var prefix="<tr><td"+(c>1?" colspan=\""+c+"\"":"")+"><div class=\"superTableSubTableClose\"><a href=\"#\">▲</a></div>";
 				var suffix=c===colCnt?"</td></tr>":"</td><td"+(colCnt-c>1?" colspan=\""+(colCnt-c)+"\"":"")+">&nbsp;</td></tr>";
 				var st=$(prefix+subTable+suffix)
 					.insertAfter(row)
-					.addClass("subTable");
+					.addClass("superTableSubTable");
 				subRowCache[rows.index(row)][type]=st;
 				var stt=st.children("td:first")
 					.addClass("highlight")
@@ -231,7 +293,7 @@
 		function subTableClose(subRow){
 			var row,highlight=false;
 			row=subRow.prev();
-			while(row.is(".subTable")){
+			while(row.is(".superTableSubTable")){
 				row=row.prev();
 			}
 			subRow.hide();
@@ -258,7 +320,7 @@
 			}
 			else{
 				if(targ.is("div")){
-					if(targ.hasClass("subTableClose")){
+					if(targ.hasClass("superTableSubTableClose")){
 						subTableClose(targ.closest("tr"));
 					}
 				}
@@ -304,6 +366,7 @@
 			table.click(click);
 		}
 		table=$(this);
+		table.addClass("superTable");
 		rows=table.find("tbody > tr");
 		goodlooking();
 		clicking();
@@ -311,76 +374,18 @@
 		searchable();
 	});
 };
-function htmlEncode(s){
-	if(!s){
-		return "&nbsp;";
-	}
-	var s1="",s2=s.toString(),i=s2.length;
-	for(var j=0;j<i;j++){
-		var c=s2.charAt(j);
-		if(c<" "||c>"~"){
-			c="&#"+c.charCodeAt()+";";
-		}
-		else {
-			switch(c){
-				case "&":
-					c="&amp;";break;
-				case "<":
-					c="&lt;";break;
-				case ">":
-					c="&gt;";break;
-				case "\"":
-					c="&quot;";break;
-				case "'":
-					c="&#39;";break;
-			}
-		}
-		s1+=c;
-	}
-	return s1;
+function Sayuser(msg){
+	$("#lblSayuser").text(msg);
 }
-function wardsDrill(tr,callback){
-	var borough=tr.children("td:first").text();
-	if(borough){
-		$.ajax({
-			url:"ACOAssaultsWardsPerBorough.ashx",
-			cache:false,
-			data:"borough="+borough,
-			dataType:"json",
-			success:function(data){
-				var s="";
-				$.each(data,function(i,row){
-					s+="<tr><td>"+htmlEncode(row.Ward)+"</td><td>"+htmlEncode(row.Assaults)+"</td></tr>";
-				});
-				s="<table cellspacing=\"0\"><thead><tr><th>Ward</th><th class=\"number\">Number of Assaults</th></tr></thead><tbody>"+s+"</tbody></table>";
-				callback(s,2);
-			}
-		});
-	}
+function DebugSayuser(msg){
+	$("#lblDebugSayuser").text(msg);
 }
-function monthsDrill(tr,callback){
-	var borough=tr.children("td:first").text();
-	if(borough){
-		$.ajax({
-			url:"ACOAssaultsMonthsPerBorough.ashx",
-			cache:false,
-			data:"borough="+borough,
-			dataType:"json",
-			success:function(data){
-				var s="";
-				$.each(data,function(i,row){
-					s+="<tr><td>"+htmlEncode(row.Month)+"</td><td>"+htmlEncode(row.Assaults)+"</td></tr>";
-				});
-				s="<table cellspacing=\"0\"><thead><tr><th class=\"month\">Month</th><th class=\"number\">Number of Assaults</th></tr></thead><tbody>"+s+"</tbody></table>";
-				callback(s,2);
-			}
-		});
-	}
+function ShowProgress(onoff) {
+  if(onoff)
+      $("#lblProgress").html("<img src='images/progress.gif' width='13' height='13'/>");
+  else
+      $("#lblProgress").html("");
 }
 $(function(){
-	$("#boroughTable").superTable({
-		"searchable":{"searchCtl":"#boroughSearch","searchColumn":1},
-		"clickable":[{"column":3,"superFunc":"subTable","superFuncFunc":wardsDrill},
-			{"column":2,"superFunc":"subTable","superFuncFunc":monthsDrill}]
-	});
+	makeNavMenu();
 });
