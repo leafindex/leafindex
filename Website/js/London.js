4074c7d8ac0edea89e8bfeb4c014500cb2ce5fc0
Jonathan Clarke <jonathan@bitwix.com>
2010-02-27 12:06:23 +0000

    Making a Population object

--- Website/js/London.js
+++ Website/js/London.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
     LoadKmlMaps();
     createData();
     FloatingInfoBoxHide();
+    //AJCTest();
 });
 function createData(){
 	var args="arg1="+encodeURIComponent(SelectedTabName());
@@ -201,8 +202,8 @@ function DrawDistribution() {
     var stats0, stats1;
     var width, height, x, series_count, idx, series_width, bidx, b, pathstr, elt, bcolour, left_margin, right_margin;
 
-    stats0 = GetPopStatsFromArrays(_borough_data, DIST_COL0);
-    stats1 = GetPopStatsFromArrays(_borough_data, DIST_COL1);
+    stats0 = new Population(_borough_data, DIST_COL0);
+    stats1 = new Population(_borough_data, DIST_COL1);
     _do_distribution = true;
     
     width = 1000;
@@ -260,58 +261,58 @@ function MakeXAxisTicks(paper, stats, zero_pos, max_pos, margin) {
 
     ticklen = 5;
     gap = 30;
-    pos0 = DistribPos(stats[MIN_IDX], stats, zero_pos, max_pos);
-    pos1 = DistribPos(stats[MAX_IDX], stats, zero_pos, max_pos);
+    pos0 = DistribPos(stats.MinVal(), stats, zero_pos, max_pos);
+    pos1 = DistribPos(stats.MaxVal(), stats, zero_pos, max_pos);
     paper.path(MakePathString(pos0, margin, pos1, margin ));
 
-    paper.text(pos0, margin + ticklen + gap, stats[MIN_IDX] + "\nMinimum");
-    paper.text(pos1, margin + ticklen + gap, stats[MAX_IDX] + "\nMaximum");
+    paper.text(pos0, margin + ticklen + gap, stats.MinVal() + "\nMinimum");
+    paper.text(pos1, margin + ticklen + gap, stats.MaxVal() + "\nMaximum");
 
     paper.path(MakePathString(pos0, margin + ticklen, pos0, margin));
     paper.path(MakePathString(pos1, margin + ticklen, pos1, margin));
 
-    pos = DistribPos(stats[MEDIAN_IDX], stats, zero_pos, max_pos);
+    pos = DistribPos(stats.Median(), stats, zero_pos, max_pos);
     paper.path(MakePathString(pos, margin + ticklen, pos, margin));
-    paper.text(pos, margin + ticklen + gap, stats[MEDIAN_IDX] + "\nMedian");
+    paper.text(pos, margin + ticklen + gap, stats.Median() + "\nMedian");
 
-    pos = DistribPos(stats[LQ_IDX], stats, zero_pos, max_pos);
+    pos = DistribPos(stats.LowerQuartile(), stats, zero_pos, max_pos);
     paper.path(MakePathString(pos, margin + ticklen, pos, margin));
-    paper.text(pos, margin + ticklen + gap, stats[LQ_IDX] + "\nLower\nQuartile");
+    paper.text(pos, margin + ticklen + gap, stats.LowerQuartile() + "\nLower\nQuartile");
 
-    pos = DistribPos(stats[UQ_IDX], stats, zero_pos, max_pos);
+    pos = DistribPos(stats.UpperQuartile(), stats, zero_pos, max_pos);
     paper.path(MakePathString(pos, margin + ticklen, pos, margin));
-    paper.text(pos, margin + ticklen + gap, stats[UQ_IDX] + "\nUpper\nQuartile");
+    paper.text(pos, margin + ticklen + gap, stats.UpperQuartile() + "\nUpper\nQuartile");
 }
 
 function MakeYAxisTicks(paper, stats, zero_pos, max_pos, margin) {
     var pos0, pos1, pos, ticklen;
 
     ticklen = 5;
-    pos0 = DistribPos(stats[MIN_IDX], stats, zero_pos, max_pos);
-    pos1 = DistribPos(stats[MAX_IDX], stats, zero_pos, max_pos);
+    pos0 = DistribPos(stats.MinVal(), stats, zero_pos, max_pos);
+    pos1 = DistribPos(stats.MaxVal(), stats, zero_pos, max_pos);
     paper.path(MakePathString(margin, pos0, margin, pos1));
 
-    paper.text(margin / 2, pos0, stats[MIN_IDX] + " Min");
-    paper.text(margin / 2, pos1, stats[MAX_IDX] + " Max");
+    paper.text(margin / 2, pos0, stats.MinVal() + " Min");
+    paper.text(margin / 2, pos1, stats.MaxVal() + " Max");
 
     paper.path(MakePathString(margin - ticklen, pos0, margin, pos0));
     paper.path(MakePathString(margin - ticklen, pos1, margin, pos1));
     
-    pos = DistribPos(stats[MEDIAN_IDX], stats, zero_pos, max_pos);
+    pos = DistribPos(stats.Median(), stats, zero_pos, max_pos);
     paper.path(MakePathString(margin - ticklen, pos, margin, pos));
-    paper.text(margin / 2, pos, stats[MEDIAN_IDX] + " Med");
+    paper.text(margin / 2, pos, stats.Median() + " Med");
 
-    pos = DistribPos(stats[LQ_IDX], stats, zero_pos, max_pos);
+    pos = DistribPos(stats.LowerQuartile(), stats, zero_pos, max_pos);
     paper.path(MakePathString(margin - ticklen, pos, margin, pos));
-    paper.text(margin / 2, pos, stats[LQ_IDX] + " LQ");
-    
-    pos = DistribPos(stats[UQ_IDX], stats, zero_pos, max_pos);
+    paper.text(margin / 2, pos, stats.LowerQuartile() + " LQ");
+
+    pos = DistribPos(stats.UpperQuartile(), stats, zero_pos, max_pos);
     paper.path(MakePathString(margin - ticklen, pos, margin, pos));
-    paper.text(margin / 2, pos, stats[UQ_IDX] + " UQ");
+    paper.text(margin / 2, pos, stats.UpperQuartile() + " UQ");
 } 
 
 function DistribPos(val, stats, zero_pos, max_pos) {
-    var ratio = val / stats[MAX_IDX];   //(val - stats[MIN_IDX]) / (stats[MAX_IDX] - stats[MIN_IDX]);
+    var ratio = val / stats.MaxVal();   //(val - stats[MIN_IDX]) / (stats[MAX_IDX] - stats[MIN_IDX]);
     return Math.floor(zero_pos + ratio * (max_pos - zero_pos));
 }
 
@@ -459,33 +460,78 @@ function GetColour(i) {
 }
 
 // array comprising Min, Max, Mean, Median, LowerQuartile, UpperQuartile in that order
-var MIN_IDX = 0;
-var MAX_IDX = 1;
-var MEAN_IDX = 2;
-var MEDIAN_IDX = 3;
-var LQ_IDX = 4;
-var UQ_IDX = 5;
+//var MIN_IDX = 0;
+//var MAX_IDX = 1;
+//var MEAN_IDX = 2;
+//var MEDIAN_IDX = 3;
+//var LQ_IDX = 4;
+//var UQ_IDX = 5;
 
-function GetPopStatsFromArrays(dataset, column_idx) {
-    var i, array = [];
+//function GetPopStatsFromArrays(dataset, column_idx) {
+//    var i, array = [];
+//    for (i = 0; i < dataset.length; i++)
+//        array[i] = dataset[i][column_idx];
+//    return GetPopStats(array);    
+//}
+
+//function GetPopStats(array) {
+//    var i, total, result = new Array(0, 0, 0, 0, 0, 0);
+//    if (array.length == 0)
+//        return result;
+//    var sorted_array = array.sort(function(a, b) { return a - b });
+//    result[MIN_IDX] = sorted_array[0];
+//    result[MAX_IDX] = sorted_array[sorted_array.length - 1];
+//    result[MEDIAN_IDX] = sorted_array[Math.floor(sorted_array.length / 2)];
+//    result[LQ_IDX] = sorted_array[Math.floor(sorted_array.length / 4)];
+//    result[UQ_IDX] = sorted_array[Math.floor(sorted_array.length * 3 / 4)];
+//    total = 0;
+//    for (i = 0; i < sorted_array.length; i++)
+//        total += sorted_array[i];
+//    result[MEAN_IDX] = total / sorted_array.length;            
+//    return result;
+//}
+
+//function AJCTest() {
+//    var i = 0, test = [];
+//    test[i++] = new Array(1, 14);
+//    test[i++] = new Array(2, 27);
+//    test[i++] = new Array(3, 18);
+//    test[i++] = new Array(3, 10);
+//    test[i++] = new Array(3, 15);
+//    test[i++] = new Array(3, 19);
+//    test[i++] = new Array(3, 27);
+//    var pop = new Population(test, 1);
+//    Sayuser("AJCTest " + pop.MinVal() + " " + pop.LowerQuartile() + " " + pop.Median() 
+//        + " " + pop.UpperQuartile() + " " + pop.MaxVal()  + " mean=" + pop.Mean() );
+//}
+
+function Population(dataset, column_idx) {
+    var i, temparray, total;
+    temparray = [];
     for (i = 0; i < dataset.length; i++)
-        array[i] = dataset[i][column_idx];
-    return GetPopStats(array);    
-}
-
-function GetPopStats(array) {
-    var i, total, result = new Array(0, 0, 0, 0, 0, 0);
-    if (array.length == 0)
-        return result;
-    var sorted_array = array.sort(function(a, b) { return a - b });
-    result[MIN_IDX] = sorted_array[0];
-    result[MAX_IDX] = sorted_array[sorted_array.length - 1];
-    result[MEDIAN_IDX] = sorted_array[Math.floor(sorted_array.length / 2)];
-    result[LQ_IDX] = sorted_array[Math.floor(sorted_array.length / 4)];
-    result[UQ_IDX] = sorted_array[Math.floor(sorted_array.length * 3 / 4)];
+        temparray[i] = dataset[i][column_idx];
+    this._array = temparray.sort(function(a, b) { return a - b });
     total = 0;
-    for (i = 0; i < sorted_array.length; i++)
-        total += sorted_array[i];
-    result[MEAN_IDX] = total / sorted_array.length;            
-    return result;
+    for (i = 0; i < this._array.length; i++)
+        total += this._array[i];
+    this._mean = total / this._array.length;  
+        
+    this.MinVal = function() {
+        return this._array[0];
+    }
+    this.MaxVal = function() {
+        return this._array[this._array.length - 1];
+    }
+    this.Median = function() {
+        return this._array[Math.floor(this._array.length / 2)];
+    }
+    this.LowerQuartile = function() {
+        return this._array[Math.floor(this._array.length / 4)];
+    }
+    this.UpperQuartile = function() {
+        return this._array[Math.floor(this._array.length * 3 / 4)];
+    }
+    this.Mean = function() {
+        return this._mean;
+    }
 }
